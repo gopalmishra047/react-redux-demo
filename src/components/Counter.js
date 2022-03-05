9798de68d7685d72922931e721a0f1fb46274ff3
gopalmishra047 <42767726+gopalmishra047@users.noreply.github.com>
2022-03-05 16:44:34 +0530

    working with multiple state properties at reducer store level

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,8 +2,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const counter = useSelector(state => state.counter);
   const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: 'INCREMENT' });
@@ -17,12 +18,14 @@ const Counter = () => {
     dispatch({ type: 'INCREASEBY', value: 5 });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'TOGGLE' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter Functional Component</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button onClick={incrementHandler}>Increment</button>
       <button onClick={increaseByHandler}>Incrase by 5</button>
       <button onClick={decrementHandler}>Decrement</button>
--- src/store/redux-store.js
+++ src/store/redux-store.js
@@ -1,17 +1,29 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = {
+  counter: 0,
+  showCounter: true
+};
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'INCREMENT') {
     return {
-      counter: state.counter + 1
+      counter: state.counter + 1,
+      showCounter: state.showCounter
     };
   } else if (action.type === 'DECREMENT') {
     return {
-      counter: state.counter - 1
+      counter: state.counter - 1,
+      showCounter: state.showCounter
     };
   } else if (action.type === 'INCREASEBY') {
     return {
-      counter: state.counter + action.value
+      counter: state.counter + action.value,
+      showCounter: state.showCounter
+    };
+  } else if (action.type === 'TOGGLE') {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter
     };
   } else {
     return state;
